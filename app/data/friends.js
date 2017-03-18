b4e5c8096a56e07b7c85344d971015fe499a07d7
kathleenzee <kathleenzelony@gmail.com>
2017-03-17 22:18:15 -0400

    take 3

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [{
+  "name":"Natsu Dragneel",
+  "photo":"http://gph.is/22YJnEc",
+  "scores":[
+     "5",
+     "5",
+     "5",
+     "5",
+     "5",
+     "5",
+     "5",
+     "5"
+   ]
+},
+{
+  "name":"Kakashi Hatake",
+  "photo":"http://gph.is/1sCN1qG",
+  "scores":[
+     "5",
+     "4",
+     "5",
+     "5",
+     "2",
+     "3",
+     "1",
+     "5"
+   ]
+},
+{
+  "name":"Light 'Kira' Yagami",
+  "photo":"http://gph.is/1o0Ti67",
+  "scores":[
+     "5",
+     "4",
+     "5",
+     "3",
+     "5",
+     "2",
+     "5",
+     "3"
+   ]
+},
+{
+  "name":"Monkey D. Luffy",
+  "photo":"http://gph.is/2cGOt3q",
+  "scores":[
+     "3",
+     "4",
+     "5",
+     "2",
+     "1",
+     "2",
+     "5",
+     "3"
+   ]
+},
+{
+  "name":"Goku",
+  "photo":"http://gph.is/2cOeWRz",
+  "scores":[
+     "2",
+     "3",
+     "4",
+     "5",
+     "2",
+     "5",
+     "4",
+     "2"
+   ]
+},
+{
+  "name":"Spike Spiegel",
+  "photo":"http://gph.is/XL46wR",
+  "scores":[
+     "4",
+     "1",
+     "1",
+     "4",
+     "3",
+     "2",
+     "1",
+     "1"
+
+   ]
+},
+{
+  "name":"Beerus",
+  "photo":"http://gph.is/2cDeSUs",
+  "scores":[
+     "4",
+     "3",
+     "1",
+     "5",
+     "5",
+     "2",
+     "3",
+     "5"
+   ]
+}];
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,26 @@
+{
+"name": "FriendFinder",
+"version": "1.0.0",
+"description": "",
+"main": "server.js",
+"scripts": {
+"test": "echo \"Error: no test specified\" && exit 1",
+"start": "node server.js"
+},
+"repository": {
+"type": "git",
+"url": "git+https://github.com/kathleenzee/FriendFinder.git"
+},
+"keywords": [],
+"author": "",
+"license": "ISC",
+"bugs": {
+"url": "https://github.com/kathleenzee/FriendFinder/issues"
+},
+"homepage": "https://github.com/kathleenzee/FriendFinder#readme",
+"dependencies": {
+"body-parser": "^1.15.2",
+"express": "^4.14.0",
+"path": "^0.12.7"
+}
+}
